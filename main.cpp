f6e4a3583e383d2b33ea70dfcae7a7a7fa8bf10b
Abdul Moiez <146338499+moiez895@users.noreply.github.com>
2024-03-21 16:14:59 +0500

    added student class

--- main.cpp
+++ main.cpp
@@ -4,6 +4,19 @@
 #include <sstream>
 using namespace std;
 
+class Student {
+public:
+    string studentID;
+    string name;
+    string email;
+    vector<Course*> coursesEnrolled;
+
+    void enrollCourse(Course* course);
+    void dropCourse(Course* course);
+    void viewCourses();
+    void save();
+    void load();
+};
 
 int main(){
 
