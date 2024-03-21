dc9b03a423d9e9474191e7fb7034193715b3bba6
Furqan Ahmad <146337498+bahriaFurqan@users.noreply.github.com>
2024-03-21 16:16:41 +0500

    added teacher and course class

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 using namespace std;
 
+class Course;
 class Student {
 public:
     string studentID;
@@ -14,6 +15,35 @@ public:
     void enrollCourse(Course* course);
     void dropCourse(Course* course);
     void viewCourses();
+    void save();
+    void load();
+};
+
+class Teacher {
+public:
+    string teacherID;
+    string name;
+    string email;
+    vector<Course*> coursesTaught;
+
+    void assignCourse(Course* course);
+    void removeCourse(Course* course);
+    void viewCourses();
+    void save();
+    void load();
+};
+
+class Course {
+public:
+    string courseCode;
+    string courseName;
+    Teacher* teacher;
+    vector<Student*> studentsEnrolled;
+    int maxCapacity = 30;
+
+    void addStudent(Student* student);
+    void removeStudent(Student* student);
+    void viewStudents();
     void save();
     void load();
 };
